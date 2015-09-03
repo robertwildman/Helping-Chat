bf8b1228fd70da6c3caef0477a715ef0cb42daf7
Robert Wildman <robertwildman@Roberts-iMac-4.default>
2015-09-04 00:02:01 +0100

    Main chat events work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,99 @@
-//This is the main css for the chat.html
+//This is the main css for the index.html
+  //var socket = io.connect('https://helpingchat.herokuapp.com:8080');
+  var socket = io.connect('localhost:8080');
+  //On connect it will start trying to set up a room for the user.
+  socket.on('connect', function(){
+	//Called when first gets contection to server
+  });
+  //This is called when a user has left the room
+  socket.on('userleft', function(){
+    //This is display the user has left div
+    $('#chat').fadeOut('slow', function() {
+    	$('#afterchat').fadeIn();
+    });
+    $('#prechat').hide();
+  });
+  //Used to display a message
+  socket.on('message', function(data){
+    //This will display a message from the other user and display it to the left of the chat screen.
+     $('<li></li>').html(data).appendTo('#chat');
+     //Makes sure the scroll keeps working.
+     document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+  });
+  //Tell user about room being empty
+  socket.on('roomempty', function(){
+    //This will display a div to let the user know that they are still wanting for a user to join them.
+    console.log('room empty');
+    $('#chat').hide();
+    $('#prechat').show();
+    $('#afterchat').hide();
+  });
+  //Used when a new user joins the room so that the chat can be displayed
+  socket.on('userjoin', function(){
+    //This is start the whole chat up and will send the username to the other user.
+    //1. Display the chat screen to the user
+    console.log('user joining');
+    $('#prechat').fadeOut('slow', function() {
+    	$('#chat').fadeIn();
+    });
+
+    $('#afterchat').hide();
+    $('#chat').fadeIn();
+    //2. Send the username to another user
+    var username = $('#username').text();
+    var stringmessage = username + " has joined the room.";
+    socket.emit('sendsystemmessage', stringmessage);
+
+  });
+  //Used for when the server wants to display to the user
+  socket.on('systemmessage', function(message){
+    //Will display the server message to the user.
+    //Server messages are place in the middle of the chat.
+    $('<li></li>').html(message).appendTo('#chat');
+     //Makes sure the scroll keeps working.
+     document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+  });
+
+
+  $(document).ready(function() {
+	$('#prechat').hide();
+    $('#chat').show();
+    $('#afterchat').hide();
+
+ 	$('#startchatbutton').click(function(event) {
+		//Will now display post infomation to the server
+		socket.emit('newuser',$('#usernameinput').val());
+		$('.indexcontainer').fadeOut(function(){
+		$('.chatcontainer').fadeIn(); });
+		$('#username').html($('#usernameinput').val());
+
+	});
+	$('#startnewchatbutton').click(function(event) {
+		//Will now display post infomation to the server
+		socket.emit('newuser',$('#usernameinput').val());
+	});
+	$('#endchatbutton').click(function(event) {
+		//Will now display post infomation to the server
+		socket.emit('userleaving');
+	$('#chat').fadeOut('slow', function() {
+    	$('#afterchat').fadeIn();
+    });
+    $('#prechat').hide();
+	});
+
+$('#messagetextbox').keypress(function(e) {
+      if(e.which == 13) {
+        $(this).blur();
+        var message = $(this).val();
+        socket.emit('sendmessage',message);
+        $('<li class="floatright"></li>').html(message).appendTo('#chat');
+     	//Makes sure the scroll keeps working.
+     	document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+        $(this).val('');
+        $(this).focus();
+      }
+    });
+
+});
+
+
--- backend.js
+++ backend.js
@@ -33,33 +33,36 @@ io.sockets.on('connection', function (socket) {
 	socket.on('newuser', function(username){
 		//This is called when a user wants to join a new room
 		//Setting up the values
-		socket.username = username;
-
+		console.log('user called newuser');
 		//Checks the array to see if its empty
 		if(freerooms.length == 0)
 		{
 			//No one is waiting
 			socket.room = getrandomroom();
 			freerooms.push(socket.room);
-			socket.join('/'+socket.room);
-			socket.emit('roomempty');
+			socket.join(socket.room);
+			console.log('user is waiting in room: ' + socket.room);
+			io.sockets.in(socket.room).emit('roomempty');
 		}
 		else
 		{
 			//We have someone.
 			socket.room = freerooms[0];
 			freerooms.splice(0, 1);
-			socket.join('/'+socket.room);
+			socket.join(socket.room);
 			takenrooms.push(socket.room);
+			console.log('user has found someone in room: ' + socket.room);
 			socket.broadcast.to(socket.room).emit('userjoin');
+    		var stringmessage = username + " has joined the room.";
+			socket.broadcast.to(socket.room).emit('systemmessage',stringmessage);
 		}
 	});
 	//When the user is leaving a room.
 	socket.on('userleaving', function(){
 		//Tells old user that this user has left and then emits leaving function
-		socket.broadcast.to('/'+socket.room).emit('systemmessage', 'User has left.');
-		socket.broadcast.to('/'+socket.room).emit('userleft');
-		socket.leave('/'+socket.room);
+		socket.broadcast.to(socket.room).emit('systemmessage', 'User has left.');
+		socket.broadcast.to(socket.room).emit('userleft');
+		socket.leave(socket.room);
 		//Removes the room from the taken list.
 		var index = takenrooms.indexOf(socket.room);
 		if (index > -1) {
@@ -69,9 +72,9 @@ io.sockets.on('connection', function (socket) {
 	//When the user disconnects
 	socket.on('disconnect', function(){
 		//Tells old user that this user has left and then emits leaving function
-		socket.broadcast.to('/'+socket.room).emit('systemmessage', 'User has left.');
-		socket.broadcast.to('/'+socket.room).emit('userleft');
-		socket.leave('/'+socket.room);
+		socket.broadcast.to(socket.room).emit('systemmessage', 'User has left.');
+		socket.broadcast.to(socket.room).emit('userleft');
+		socket.leave(socket.room);
 		//Removes the room from the taken list.
 		var index = takenrooms.indexOf(socket.room);
 		if (index > -1) {
@@ -80,11 +83,11 @@ io.sockets.on('connection', function (socket) {
 	});
 	socket.on('sendsystemmessage', function(message)
 	{
-		ssocket.broadcast.to('/'+socket.room).emit('systemmessage',message);
+		socket.broadcast.to(socket.room).emit('systemmessage',message);
 	});
 	socket.on('sendmessage', function(message)
 	{
-		ssocket.broadcast.to('/'+socket.room).emit('message',message);
+		socket.broadcast.to(socket.room).emit('message',message);
 	});
 
 
@@ -94,7 +97,7 @@ io.sockets.on('connection', function (socket) {
 function getrandomroom()
 {
 	var randomnumber = Math.floor(Math.random()*10000000);
-	if(takenrooms.indexOf(randomnumber) > -1 ))
+	if(takenrooms.indexOf(randomnumber) > -1 )
 	{
 		return getrandomroom();
 	}
